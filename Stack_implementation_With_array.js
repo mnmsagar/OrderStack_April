d2a93f15f327025f4c25aa4024d32c95b1fdc9bf
mnmsagar <54708901+mnmsagar@users.noreply.github.com>
2023-04-06 12:44:37 +0530

    Update Stack_implementation_With_array.js

--- Stack_implementation_With_array.js
+++ Stack_implementation_With_array.js
@@ -1,66 +1,85 @@
 class Stack {
     constructor(n) {
-        this.items = [];
-        this.currentSize = this.items.length;
-        this.maxSize=n;
+      this.items = [];
+      this.currentSize = 0;
+      this.maxSize = n;
     }
-
+  
     push(element) {
-        if(this.currentSize>=this.maxSize){
-            console.log("Stack is full");
-        }else{
-            this.items.push(element);
-            this.currentSize++;
-        }
-     
+      if (this.currentSize >= this.maxSize) {
+        return "Stack is Full";
+      } else {
+        this.items.push(element);
+        this.currentSize++;
+      }
     }
-
+  
     pop() {
-        if (this.items.length === 0) {
-            return "Underflow";
-        }else{
-            return this.items.pop();
-        }
-        
+      if (this.currentSize === 0) {
+        return "Underflow";
+      } else {
+        this.currentSize--;
+        return this.items.pop();
+      }
     }
-
+  
     peek() {
-        if(this.currentSize>0){
-            return this.items[this.items.length - 1];
-        }
-        else{
-            console.log("Stack is Empty");
-        }
+      if (this.currentSize > 0) {
+        return this.items[this.items.length - 1];
+      } else {
+        return "Stack is Empty";
+      }
     }
-
+  
     size() {
-        if(this.currentSize>0 && this.currentSize<=this.maxSize){
-            return this.items.length;
-        }else{
-            console.log("Stack is Empty");
-        }
-        
+      return this.currentSize;
     }
-
+  
     isEmpty() {
-        if(this.currentSize>0 && this.currentSize<=this.maxSize){
-            return this.items.length === 0;
-        }else if(this.currentSize === 0){
-            console.log("Stack is Empty");
-        }
-       
+      return this.currentSize === 0;
     }
-
+  
     clear() {
-        if(this.currentSize === 0){
-            console.log("Stack is already Empty");
-        }else{
-            this.items = [];
-        }
-       
+      this.currentSize = 0;
     }
-}
+  }
 
 
-// let stack1= new Stack(maxSize);
+// Test Cases
 
+  // Create a new stack with a maximum size of 3
+const stack = new Stack(3);
+
+// Test push method
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.size()); // Expected output: 3
+console.log(stack.push(4)); // Expected output: "Stack is Full"
+
+// Test pop method
+console.log(stack.pop()); // Expected output: 3
+console.log(stack.pop()); // Expected output: 2
+console.log(stack.pop()); // Expected output: 1
+console.log(stack.pop()); // Expected output: "Underflow"
+
+// Test peek method
+console.log(stack.peek()); // Expected output: "Stack is Empty"
+stack.push(5);
+console.log(stack.peek()); // Expected output: 5
+
+// Test size method
+console.log(stack.size()); // Expected output: 1
+
+// Test isEmpty method
+console.log(stack.isEmpty()); // Expected output: false
+stack.pop();
+console.log(stack.isEmpty()); // Expected output: true
+
+// Test clear method
+stack.push(6);
+stack.push(7);
+console.log(stack.size()); // Expected output: 2
+stack.clear();
+console.log(stack.size()); // Expected output: 0
+console.log(stack.isEmpty()); // Expected output: true
